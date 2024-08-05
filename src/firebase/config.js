4665d9e0a2555c0356ca0b8184a3c5fe7f051916
Nguyen Ngoc Hai <hainn2009@gmail.com>
2024-08-05 22:12:34 +0700

    fourth

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,6 +4,15 @@ import 'firebase/analytics';
 import 'firebase/auth';
 import 'firebase/firestore';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCzkxgHPe9Q-PF5WAejXg4WV2tTRQ6YuEw",
+  authDomain: "fun-chat-e6095.firebaseapp.com",
+  projectId: "fun-chat-e6095",
+  storageBucket: "fun-chat-e6095.appspot.com",
+  messagingSenderId: "1080195917383",
+  appId: "1:1080195917383:web:729f44d293e8634ddc3362",
+  measurementId: "G-P634MNXX6K"
+};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
