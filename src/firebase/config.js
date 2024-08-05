6dfc2c1fd38f9f748018a9b2a2131a0b57518f66
Nguyen Ngoc Hai <hainn2009@gmail.com>
2024-08-05 21:23:13 +0700

    first

---
+++ src/firebase/config.js
@@ -0,0 +1,39 @@
+import firebase from 'firebase/app';
+
+import 'firebase/analytics';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+// var firebaseConfig = {
+//   apiKey: 'AIzaSyBRpDZ63mVfrzmzTjHFjRssP4cOvw_jc04',
+//   authDomain: 'chat-app-c975a.firebaseapp.com',
+//   projectId: 'chat-app-c975a',
+//   storageBucket: 'chat-app-c975a.appspot.com',
+//   messagingSenderId: '942648297216',
+//   appId: '1:942648297216:web:d8c4321ba778c8808f95e5',
+//   measurementId: 'G-01WLMKYFFM',
+// };
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCzkxgHPe9Q-PF5WAejXg4WV2tTRQ6YuEw",
+  authDomain: "fun-chat-e6095.firebaseapp.com",
+  projectId: "fun-chat-e6095",
+  storageBucket: "fun-chat-e6095.appspot.com",
+  messagingSenderId: "1080195917383",
+  appId: "1:1080195917383:web:729f44d293e8634ddc3362",
+  measurementId: "G-P634MNXX6K"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+if (window.location.hostname === 'localhost') {
+  auth.useEmulator('http://localhost:9099');
+  db.useEmulator('localhost', '8080');
+}
+
+export { db, auth };
+export default firebase;
